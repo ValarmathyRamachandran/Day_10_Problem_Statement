d900fa751433b6dd097211fb125888b064a34d57
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-09-18 19:32:50 +0530

    computations of variables are stored

--- sortingArray.sh
+++ sortingArray.sh
@@ -2,5 +2,13 @@ read -p "Enter value a: " a;
 read -p "Enter value b: " b;
 read -p "Enter value c: " c;
 
-echo $a $b $c
+computation1=$(( a*b+c ));
+computation2=$(( a*b+c ));
+computation3=$(( c+a/b ));
+computation4=$(( a%b+c ));
+
+echo $a $b $c;
+echo $computation1 $computation2 $computation3 $computation4;
+
+
 
